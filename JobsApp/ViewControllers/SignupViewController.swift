0848e619ee57d9219dc6a42a2834c3e184dbc6df
Varya Utkina <varya2412@mail.ru>
2024-11-15 12:03:02 +0300

    Create SignupVC

---
+++ JobsApp/ViewControllers/SignupViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignupViewController.swift
+//  JobsApp
+//
+//  Created by Варвара Уткина on 15.11.2024.
+//
+
+import UIKit
+
+final class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
